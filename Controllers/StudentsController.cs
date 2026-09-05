b2e424e280d566515c6876e72d5238623d0bf291
Monika Hossain <monikahossain25@gmail.com>
2026-09-05 15:42:22 +0600

    Add project files.

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroRentApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentNames =
+            {
+                "Lira",
+                "Doxygen",
+                "Copilot",
+                "GitHub"
+            };
+
+            return Ok(studentNames);
+        }
+    }
+}
