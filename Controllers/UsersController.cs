9c933c2deaf79df456ae33e6441a6f989ad510e7
Monika Hossain <monikahossain25@gmail.com>
2026-09-05 20:01:03 +0600

    Add Users controller with GET endpoint

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using MicroRentApi.Models.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroRentApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var users = new List<User>()
+            {
+                new User
+                {
+                    Id = Guid.NewGuid(),
+                    Code = "U001",
+                    Name = "Monika Hossain",
+                    UserImageUrl = "https://example.com/images/monika.jpg"
+                },
+                new User
+                {
+                    Id = Guid.NewGuid(),
+                    Code = "U003",
+                    Name = "Hossain Monika",
+                    UserImageUrl = "https://example.com/images/hossain.jpg"
+                },
+            };
+            return Ok(users);
+        }
+    }
+}
