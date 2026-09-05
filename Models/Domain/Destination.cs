49190d0fd131ec9928e5ccc3359229b15744b900
Monika Hossain <monikahossain25@gmail.com>
2026-09-05 15:46:32 +0600

    Add core domain models for destinations and trails

---
+++ Models/Domain/Destination.cs
@@ -0,0 +1,13 @@
+﻿namespace MicroRentApi.Models.Domain
+{
+    public class Destination
+    {
+        public Guid Id { get; set; }
+
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+
+        public string? ImageUrl { get; set; }
+    }
+}
---
+++ Models/Domain/TrailDifficulty.cs
@@ -0,0 +1,10 @@
+﻿namespace MicroRentApi.Models.Domain
+{
+    public class TrailDifficulty
+    {
+
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
