ae1dce1b1a9fd572d30996c96602f36ef350e4ac
Monika Hossain <monikahossain25@gmail.com>
2026-09-06 16:18:06 +0600

    Create DTO folder and create UserDto model

---
+++ Models/DTO/UserDto.cs
@@ -0,0 +1,13 @@
+﻿namespace MicroRentApi.Models.DTO
+{
+    public class UserDto
+    {
+        public Guid Id { get; set; }
+
+        public string Code { get; set; } = string.Empty;
+
+        public string Name { get; set; } = string.Empty;
+
+        public string? UserImageUrl { get; set; }
+    }
+}
